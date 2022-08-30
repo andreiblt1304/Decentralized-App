b50541f89920e5bfcdc041da6c0c663c159dae03
andreiblt <andreibaltariu1304@gmail.com>
2022-08-31 01:13:14 +0300

    Modified Navbar and optimized it for mobile use

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import{ HiMenuAlt4 } from 'react-icons/hi';
-import{ AiOutlineClose } from 'react-icons/ai';
+import { HiMenu, HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 import logo from '../images/logo.png';
 
 const NavbarItem = ({title, classProps}) => {
@@ -26,11 +26,24 @@ const Navbar = () => {
                     Login
                 </li>
             </ul>
-            {/* <div className='flex relative'>
+            <div className='flex relative'>
                 {toggleMenu 
                     ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/> 
                     : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>}
-            </div> */}
+                {toggleMenu && (
+                    <ul
+                        className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                                    flex flex-col justify-start items-end rounded-md blue-glassmorphism
+                                    text-white animate-slide-in">
+                        <li className='text-xl w-full my-2'>
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {["NFT Market", "Transfer", "Tutorials", "About me"].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </nav>
     );
 }
